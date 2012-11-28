f0c6c5927134d15139bb75e4d4dfcc732ee454f6
jeff-k <jak19@sfu.ca>
2012-11-28 13:27:00 -0800

    added samtools as a submodule

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from distutils.core import setup, Extension
+samtools = ['samtools/kstring.c', 'samtools/sam_header.c', 'samtools/razf.c',
+            'samtools/bgzf.c', 'samtools/kaln.c', 'samtools/kprobaln.c',
+            'samtools/bam.c', 'samtools/faidx.c', 'samtools/sam.c',
+            'samtools/bam_pileup.c','samtools/bam_index.c',
+            'samtools/bam_aux.c', 'samtools/bam_import.c',
+            'samtools/bam_md.c', 'samtools/errmod.c']
 
 tactmod = Extension('tactmod',
         sources = ['src/base.c', 'src/column.c', 'src/fasta.c', 'src/bam.c',
                    'src/tactmod_init.c'],
 
-        include_dirs = ['include/samtools'],
-        libraries = ['bam'])
+        library_dirs = [],
+        include_dirs = ['samtools/','samtools/bcftools','include/samtools'],
+        libraries = ['m', 'z'])
 
 setup (name = 'tactmod',
         version = '0.1',
