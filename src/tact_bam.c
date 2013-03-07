8938d83da60b8e8630cbb68f50f8dba1c4238b46
jeff-k <jak19@sfu.ca>
2013-03-06 16:24:45 -0800

    FIFO buffer type

--- src/tact_bam.c
+++ src/tact_bam.c
@@ -13,7 +13,7 @@ PyMethodDef Bam_methods[] = {
 //                "columns covering a range"},
 //    {"pileup", (PyCFunction)Bam_pileup, METH_VARARGS,
 //                "pileups covering a range"},
-    {"tuple", (PyCFunction)Bam_tuple, METH_VARARGS,
+    {"counts", (PyCFunction)Bam_counts, METH_VARARGS,
                 "fixed size tuples"},
     {NULL}
 };
@@ -52,7 +52,11 @@ PyTypeObject tactmod_BamIterType = {
 
 PyObject *
 tactmod_BamIter_iter(PyObject *self) {
+    // Initialize pileup buffer
+    tactmod_BamIter *s = (tactmod_BamIter *)self;
+    s->pileup = bam_plbuf_init(pileup_func, s);
     Py_INCREF(self);
+    trace("launching iterator");
     return self;
 }
 
@@ -66,50 +70,64 @@ tactmod_BamIter_next(PyObject *self) {
 //    pileup_buffer buffer;
     tactmod_BamIter *iterator = (tactmod_BamIter *)self; 
     tactmod_BamObject *bam = iterator->bam;
+    bam_plbuf_t *pileup = iterator->pileup;
     PyTupleObject *tuple;
     PyTupleObject *features;
     PyIntObject *value;
-    while((iterator->position) < iterator->stop) {
-        tuple = PyTuple_New(5);
-        Py_INCREF(tuple);
-        
-        value = PyInt_FromLong(iterator->position);
-        Py_INCREF(value);
-        PyTuple_SET_ITEM(tuple, 0, value); 
 
-        for (i = 1; i < 5; i++) {
-            features = PyTuple_New(4);
-            Py_INCREF(features);
-            value = PyInt_FromLong(0);
-            Py_INCREF(value);
-            for (j = 0; j < 4; j++) {
-                PyTuple_SET_ITEM(features, j, value);
-            }
-            PyTuple_SET_ITEM(tuple, i, features);
-        }
+// Add tuples to an array for every completed position in the pileup
 
-        start = iterator->position + 1;
-//        PyObject *arglist = Py_BuildValue("(i)", start);
-//        buffer.pileup = PyObject_CallObject((PyObject*)&tactmod_ColumnType,
-//                                        arglist);
-//        buffer.depth = 0;
-//        buffer.buf = bam_plbuf_init(pileup_func, buffer.pileup);
-//        iterator->position = buffer.pileup->position;
-        iterator->position++;
-        end = start + 1;
-        tid = 0;
-        status = bam_fetch(bam->fd->x.bam, bam->idx, tid,
-                       start, end, tuple, fetch_stats);
-//        bam_plbuf_push(0, buffer.buf);
-//        bam_plbuf_destroy(buffer.buf);
-//        if (buffer.pileup->depth == 0) {
-//            continue;
-//        }
-        return tuple;
+//    trace("entering next iteration");
+//    trace("\n\t\tposition:\t%d\n\t\tbuffer start:\t%d\n\t\tbuffer end:\t%d\n\t\tbuf offset:\t%d",
+//            iterator->position, iterator->buffer_start, iterator->buffer_end, 
+//            iterator->offset);
+    if (iterator->position >= iterator->stop) {
+        trace("iteration over");
+        bam_plbuf_push(NULL, pileup);
+        bam_plbuf_destroy(pileup);
+//        Py_DECREF(iterator->buffer);
+        PyErr_SetNone(PyExc_StopIteration);
+        return NULL;
     }
 
-    PyErr_SetNone(PyExc_StopIteration);
-    return NULL;
+    trace("next iterative step");
+
+    PyObject *tuples;
+//    int buffer_size = 25;
+ //
+ //   trace("next iteration:\t%d\t%d\t%d", iterator->position, iterator->offset, iterator->buffer_pos);
+    if (0) {
+//    if ((iterator->offset == iterator->buffer_length) || (iterator->position >= iterator->buffer_pos)) {
+        trace("replenishing buffer");
+//        while((iterator->buffer_length < buffer_size) && (iterator->buffer_pos < iterator->stop)) {
+ //       trace("condition hit");
+        iterator->offset = 0;
+
+         bam_fetch(bam->fd->x.bam, bam->idx, 0,
+                  0, 100, (void *)self, fetch_f);
+        // top off the buffer (as per samtools doc)
+        bam_plbuf_push(NULL, pileup);
+        bam_plbuf_reset(pileup);
+//        trace("%d - %d", iterator->buffer_start, iterator->buffer_end); 
+  //      trace("done replenishing buffer");
+    }
+//    trace("\n\t\tposition:\t%d\n\t\tbuffer start:\t%d\n\t\tbuffer end:\t%d\n\t\tbuf offset:\t%d\n\t\tbuffer size:\t%d",
+//            iterator->position, iterator->buffer_start, iterator->buffer_end, 
+//            iterator->offset, iterator->buffer_length);
+
+
+    
+//    trace("position %d\tstop %d", iterator->position, iterator->stop);
+//    trace("buffer size %d", PySequence_Size(iterator->buffer));
+
+    trace("%d\t%d", iterator->position, iterator->offset);
+//    tuple = PyList_GET_ITEM(iterator->buffer, iterator->offset);
+    tuple = Py_None;
+    Py_INCREF(tuple);
+    (iterator->offset)++;
+    (iterator->position)++;
+    return tuple;
+
 }
 
 void
@@ -155,83 +173,17 @@ Bam_init(tactmod_BamObject *self, PyObject *args, PyObject *kwds) {
 
 
 PyObject *
-Bam_tuple(tactmod_BamObject *self, PyObject *args) {
-    return NULL;
-}
-
-PyObject *
 Bam_jump(tactmod_BamObject *self, PyObject *args) {
-    int start, end, tid, status; 
-
-    tid = 0;
-    pileup_buffer buffer;
-    PyObject *contig;
-    buffer.filter == NULL;
-    buffer.populate_bases = 1;
-    if (!PyArg_ParseTuple(args, "ii|O", &tid, &start, &buffer.filter)) {
-        return NULL;
-    }
-    end = start + 1;
-
-    buffer.pileup = NULL;
-
-    if (buffer.filter != NULL) {
-        if (!PyCallable_Check(buffer.filter)) {
-            PyErr_SetString(PyExc_TypeError,
-                            "filter callback is not callable");
-            buffer.filter = Py_None;
-        }
-    }
-    Py_INCREF(buffer.filter);
-    if (self->idx == 0) {
-        printf("Bam index is not available\n");
-        return NULL;
-    }
-    if (self->fd == 0) {
-        printf("Bam could not be loaded\n");
-        return NULL;
-    }
-
-    if (tid < 0) {
-        printf("invalid region\n");
-        return NULL;
-    }
-
-    PyObject *arglist = Py_BuildValue("(i)", start);
-    buffer.pileup = PyObject_CallObject((PyObject*)&tactmod_ColumnType,
-                                        arglist);
-    buffer.depth = 0;
-    buffer.buf = bam_plbuf_init(pileup_func, buffer.pileup);
-    status = bam_fetch(self->fd->x.bam, self->idx, tid,
-                       start, end, &buffer, fetch_column);
-    bam_plbuf_push(0, buffer.buf);
-    bam_plbuf_destroy(buffer.buf);
-    return buffer.pileup;
+    return Py_None;
 }
 
 PyObject *
 Bam_slice(tactmod_BamObject *self, PyObject *args) {
-    tactmod_BamIter *iter;
-    int tid, start, stop;
-    pileup_buffer buffer;
-    PyObject *contig;
-    buffer.filter = NULL;
-    buffer.populate_bases = 1;
-    if (!PyArg_ParseTuple(args, "iii|O", &tid, &start, &stop, &buffer.filter)) 
-    {
-        return NULL;
-    }
-    iter = (tactmod_BamIter *)PyObject_New(tactmod_BamIter, 
-                                           &tactmod_BamIterType);
-    iter->bam = self;
-    iter->offset = 0;
-    iter->position = start;
-    iter->stop = stop;
-    return (PyObject *)iter;
+    return Py_None;
 }
 
 PyObject *
-Bam_stats(tactmod_BamObject *self, PyObject *args) {
+Bam_counts(tactmod_BamObject *self, PyObject *args) {
     tactmod_BamIter *iter;
     int tid, start, stop;
     
@@ -243,6 +195,7 @@ Bam_stats(tactmod_BamObject *self, PyObject *args) {
     iter->bam = self;
     iter->offset = 0;
     iter->position = start;
+    iter->start = start;
     iter->stop = stop;
     return (PyObject *)iter;
 }
@@ -315,7 +268,7 @@ static int
 fetch_stats(const bam1_t *b, void *data) {
     int i;
 //    uint8_t offset = d->pileup->position - b->core.pos;
-    uint8_t offset;
+    uint8_t offset, distance, length;
     uint8_t quality, mapping, baq, mapped, reverse, paired, duplicate;
     uint8_t base2;
     long position;
@@ -323,31 +276,39 @@ fetch_stats(const bam1_t *b, void *data) {
     PyTupleObject *features;
     PyTupleObject *tuple;
     PyIntObject *value; 
-   
+    length = 100; 
     tuple = (PyTupleObject *)data;
     //ops = b->core.n_cigar;
-    position = PyInt_AS_LONG(PyTuple_GET_ITEM(tuple, 0));
+    position = PyInt_AS_LONG(PyTuple_GET_ITEM(tuple, POSITION));
+    if ((position - b->core.pos) < 0) {
+        return 1;
+    }
     offset = position - b->core.pos;
     
     base2 = base4_base2(bam1_seqi(bam1_seq(b), offset));
+
+    //trace("base: %d", bam1_seqi(bam1_seq(b), offset));
     if (base2 > 3) {
         return 1;
     }
-    features = PyTuple_GET_ITEM(tuple, (base2 + 1));
+    features = PyTuple_GET_ITEM(tuple, base2);
     
-    value = PyTuple_GET_ITEM(features, 0);
-    old_value = PyInt_AS_LONG(value);
-    value = PyInt_FromLong(old_value++);
+//    value = PyTuple_GET_ITEM(features, 0);
+    trace("0 indexed base of seq:\t%d", bam1_seqi(bam1_seq(b), 0)); 
+
+    reverse = 1 && (b->core.flag & BAM_FREVERSE);
+    old_value = PyInt_AS_LONG(PyTuple_GET_ITEM(features, TOTAL_INDEX));
+    value = PyInt_FromLong(old_value + 1);
     Py_INCREF(value);
     PyTuple_SET_ITEM(features, TOTAL_INDEX, value);
-
+    
     old_value = PyInt_AS_LONG(PyTuple_GET_ITEM(features, QUALITY_INDEX));
     value = PyInt_FromLong(bam1_qual(b)[offset] + old_value);
     Py_INCREF(value);
     PyTuple_SET_ITEM(features, QUALITY_INDEX, value);
     
     old_value = PyInt_AS_LONG(PyTuple_GET_ITEM(features, DIRECTION_INDEX));
-    value = PyInt_FromLong(old_value + b->core.flag & BAM_FREVERSE);
+    value = PyInt_FromLong(old_value + reverse);
     Py_INCREF(value);
     PyTuple_SET_ITEM(features, DIRECTION_INDEX, value);  
     
@@ -356,6 +317,15 @@ fetch_stats(const bam1_t *b, void *data) {
     Py_INCREF(value);
     PyTuple_SET_ITEM(features, MAPPING_INDEX, value);
 
+    old_value = PyInt_AS_LONG(PyTuple_GET_ITEM(features, TAIL_DISTANCE));
+    if (reverse) {
+        distance = length - offset;
+    } else {
+        distance = offset;
+    }
+    value = PyInt_FromLong(distance + old_value);
+    Py_INCREF(value);
+    PyTuple_SET_ITEM(features, TAIL_DISTANCE, value);
     return 0;
 }
 
@@ -369,8 +339,100 @@ fetch_pileup(const bam1_t *b, void *data) {
     return 0;
 }
 
+fetch_f(const bam1_t *b, void *data) {
+    // add this alignment to the pileup buffer
+    // here would go a low lever filter
+    tactmod_BamIter *s = (tactmod_BamIter *)data;
+    bam_plbuf_t *pileup = (bam_plbuf_t *)s->pileup;
+    bam_plbuf_push(b, pileup);
+    return 0;
+}
+
 static int
 pileup_func(uint32_t tid, uint32_t pos, int n,
             const bam_pileup1_t *pl, void *data) {
+    
+    // Create the tuple to to returned for every position in the range
+    tactmod_BamIter *iterator = (tactmod_BamIter *)data;
+    PyTupleObject *tuple;
+    PyTupleObject *features;
+    PyIntObject *value;
+    int start, end;
+
+//    if ((iterator->buffer_start) > pos) {
+//        iterator->buffer_start = pos;
+//    }
+    //iterator->buffer_pos = pos;
+    int i, j;
+//    if ((pos < iterator->buffer_start) || (pos > iterator->buffer_end)) {
+//        return 0;
+//    }
+//    trace("building position for %d", pos);
+//    iterator->position = pos; 
+    tuple = PyTuple_New(6);
+    Py_INCREF(tuple);
+        
+    value = PyInt_FromLong(pos);
+    Py_INCREF(value);
+    if (1) {
+        PyTuple_SET_ITEM(tuple, POSITION, value); 
+        value = PyInt_FromLong(n);
+        PyTuple_SET_ITEM(tuple, 5, value);
+        for (i = 0; i < 4; i++) {
+            features = PyTuple_New(5);
+            Py_INCREF(features);
+            value = PyInt_FromLong(0);
+            Py_INCREF(value);
+            for (j = 0; j < 5; j++) {
+                PyTuple_SET_ITEM(features, j, value);
+            }
+            PyTuple_SET_ITEM(tuple, i, features);
+        }
+    }
+    // append tuple to list
+//    PyList_Append(buffer, (PyObject *)tuple);
+    return 0;
+}
+
+
+int ll_push(buffer_ll *list, PyTupleObject *content) {
+    buffer_node *node = malloc(sizeof(buffer_node));
+    buffer_node *old = list->head;
+    list->head = node;
+    node->next = old;
+    node->content = content;
+    if (list->size == 0) {
+        list->tail = node;
+    }
+    (list->size)++;
+    return 0;
+}
+
+PyTupleObject *ll_pop(buffer_ll *list) {
+    trace("popping linked list");
+    buffer_node *head = list->head;
+    (list->size)--;
+    Py_DECREF(head->content);
+    list->head = head->next;
+    free(head);
+
+    return list->head->content;
+}
+int ll_init(buffer_ll *list) {
+    trace("initializing linked list");
+    list = malloc(sizeof(buffer_ll));
+    list->size = 0;
+    list->end = 0;
+
+    list->tail = NULL;
+    list->head = NULL;
+    return 0;
+}
+int ll_destroy(buffer_ll *list) {
+    trace("destroying linked list");
+    while(list->size > 0) {
+        ll_pop(list);
+    }
+    free(list);
     return 0;
 }
--- src/tact_bam.h
+++ src/tact_bam.h
@@ -8,7 +8,14 @@
 #define TOTAL_INDEX 0
 #define QUALITY_INDEX   1
 #define DIRECTION_INDEX 2
-#define MAPPING_INDEX  3
+#define MAPPING_INDEX   3
+#define TAIL_DISTANCE   4
+
+#define POSITION  4
+#define MAX_INDEX   5
+#define MINOR_INDEX 6
+#define DEPTH 7
+#define INDELS  8
 
 typedef struct {
     int depth;    
@@ -23,7 +30,9 @@ typedef struct {
     PyObject *contig;
     samfile_t *fd;
     bam_index_t *idx;
-    PyObject *column_callback;
+//    bam_plbuf_t *pileup;
+//    PyObject *buffer;
+    PyObject *callback;
 } tactmod_BamObject;
 
 // Bamfile object functions
@@ -31,7 +40,6 @@ static PyObject *Bam_new(PyTypeObject *type, PyObject *args, PyObject *kwds);
 int Bam_init(tactmod_BamObject *self, PyObject *args, PyObject *kwds);
 
 PyObject *Bam_slice(tactmod_BamObject *self, PyObject *args);
-PyObject *Bam_tuple(tactmod_BamObject *self, PyObject *args);
 PyObject *Bam_counts(tactmod_BamObject *self, PyObject *args);
 PyObject *Bam_pileup(tactmod_BamObject *self, PyObject *args);
 PyObject *Bam_stats(tactmod_BamObject *self, PyObject *args);
@@ -46,12 +54,27 @@ PyTypeObject tactmod_BamType;
 // The iterator
 typedef struct {
     PyObject_HEAD
-    long int position;
-    long int offset;
-    long int start;
-    long int stop;
+    uint32_t position;
+    uint16_t offset;
+    uint32_t start;
+    uint32_t stop;
     tactmod_BamObject *bam;
+    bam_plbuf_t *pileup;
+    PyListObject *buffer; // the buffer linked list
 } tactmod_BamIter;
+
+typedef struct {
+    void *next;
+    PyObject *content;
+} buffer_node;
+
+typedef struct {
+    uint32_t size;
+    uint32_t end;
+    buffer_node *head;
+    buffer_node *tail;
+} buffer_ll;
+
 
 PyObject *tactmod_BamIter_iter(PyObject *self);
 PyObject *tactmod_BamIter_next(PyObject *self);
@@ -64,7 +87,13 @@ static int fetch_stats(const bam1_t *b, void *data);
 static int fetch_column(const bam1_t *b, void *data);
 static int fetch_pileup(const bam1_t *b, void *data);
 static int fetch_counts(const bam1_t *b, void *data);
+static int fetch_f(const bam1_t *b, void *data);
 
 static int pileup_func(uint32_t tid, uint32_t pos, int n,
                        const bam_pileup1_t *pl, void *data);
+
+int ll_push(buffer_ll *list, PyTupleObject *content);
+PyTupleObject *ll_pop(buffer_ll *list);
+int ll_init(buffer_ll *list);
+int ll_destroy(buffer_ll *list);
 #endif
